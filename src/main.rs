94038c397fd750c2205bbeb9859a9cc00bcdc2e0
smittytone <smittytone@icloud.com>
2020-01-30 22:56:12 +0000

    Edit var names

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,30 @@ extern crate num_complex;
 
 
 fn main() {
-    let imgx = 4000;
-    let imgy = 4000;
+    let img_x = 4000;
+    let img_y = 4000;
 
-    let scalex = 2.4 / imgx as f32;
-    let scaley = 2.4 / imgy as f32;
+    let scale_x = 2.4 / img_x as f32;
+    let scale_y = 2.4 / img_y as f32;
+
+    let delta = 255.0 / img_x as f32;
 
     // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    let mut img_buf = image::ImageBuffer::new(img_x, img_y);
 
     // Iterate over the coordinates and pixels of the image
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (0.04 * y as f32) as u8;
-        let b = (0.04 * x as f32) as u8;
+    // This adds the red/blue background
+    for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
+        let r = (delta * y as f32) as u8;
+        let b = (delta * x as f32) as u8;
         *pixel = image::Rgb([r, 0, b]);
     }
 
     // A redundant loop to demonstrate reading image data
-    for x in 0..imgx {
-        for y in 0..imgy {
-            let cx = y as f32 * scalex - 1.5;
-            let cy = x as f32 * scaley - 1.5;
+    for x in 0..img_x {
+        for y in 0..img_y {
+            let cx = y as f32 * scale_x - 1.0;
+            let cy = x as f32 * scale_y - 1.0;
 
             let c = num_complex::Complex::new(-0.4, 0.6);
             let mut z = num_complex::Complex::new(cx, cy);
@@ -35,7 +38,7 @@ fn main() {
                 i += 1;
             }
 
-            let pixel = imgbuf.get_pixel_mut(x, y);
+            let pixel = img_buf.get_pixel_mut(x, y);
 
             // Read RGB value of pixel
             let image::Rgb(data) = *pixel;
@@ -46,5 +49,5 @@ fn main() {
     }
 
     // Save the image as “fractal.png”, the format is deduced from the path
-    imgbuf.save("fractal.png").unwrap();
+    img_buf.save("fractal.png").unwrap();
 }
